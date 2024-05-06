bd4aee0514f4bdd108a32c052dc560be216bdc23
Arunim Sharma <arunim.sharma@bp.com>
2024-05-06 12:07:45 +0100

    intial commit

---
+++ infradetails/main.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "test-server" {
+    ami = "ami-080e449218d4434fa"
+    instance_type = "t2.micro"
+    key_name = "Test"
+    vpc_security_group_ids = [aws_security_group.test-sg.id]
+    subnet_id = aws_subnet.test-public-subnet-01.id 
+for_each = toset(["jenkins-master", "jenkins-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
+}
+
+resource "aws_security_group" "test-sg" {
+  name        = "test-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.test-vpc.id 
+  
+  ingress {
+    description      = "SHH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "test-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "test-public-subnet-01" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "test-public-subent-01"
+  }
+}
+
+resource "aws_subnet" "test-public-subnet-02" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "test-public-subent-02"
+  }
+}
+
+resource "aws_internet_gateway" "test-igw" {
+  vpc_id = aws_vpc.test-vpc.id 
+  tags = {
+    Name = "test-igw"
+  } 
+}
+
+resource "aws_route_table" "test-public-rt" {
+  vpc_id = aws_vpc.test-vpc.id 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test-igw.id 
+  }
+}
+
+resource "aws_route_table_association" "test-rta-public-subnet-01" {
+  subnet_id = aws_subnet.test-public-subnet-01.id
+  route_table_id = aws_route_table.test-public-rt.id   
+}
+
+resource "aws_route_table_association" "test-rta-public-subnet-02" {
+  subnet_id = aws_subnet.test-public-subnet-02.id 
+  route_table_id = aws_route_table.test-public-rt.id   
+}
